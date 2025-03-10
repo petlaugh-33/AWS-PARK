65b170f27315024ac26505ac32b8c52ed87cc2a3
petlaugh-33 <petlaugh@amazon.com>
2025-03-10 18:49:28 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,13 @@
-
+const CONFIG = {
+    WEBSOCKET_URL: 'wss://ocly49pex3.execute-api.us-east-1.amazonaws.com/production',
+    RESERVATIONS_API_ENDPOINT: 'https://wszwvwc915.execute-api.us-east-1.amazonaws.com/prod',
+    API_ENDPOINT: 'https://7rw5ezddjj.execute-api.us-east-1.amazonaws.com/prod',
+    RESERVATION_REFRESH_INTERVAL: 30000,
+    MAX_RECONNECT_ATTEMPTS: 5,
+    MAX_HISTORY: 10,
+    STORAGE_KEYS: {
+        HISTORY: 'parkingHistory',
+        CURRENT_STATUS: 'parkingCurrentStatus',
+        LAST_CHART_TYPE: 'lastChartType'
+    }
+};
