dd14e4c26f7ddd2b92901046728c6a2694132bc6
petlaugh-33 <petlaugh@amazon.com>
2025-03-10 18:59:09 -0400

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,13 @@
+export const RESERVATIONS_API_ENDPOINT = 'https://wszwvwc915.execute-api.us-east-1.amazonaws.com/prod';
+export const API_ENDPOINT = 'https://7rw5ezddjj.execute-api.us-east-1.amazonaws.com/prod';
+export const WEBSOCKET_URL = 'wss://ocly49pex3.execute-api.us-east-1.amazonaws.com/production';
+export const RESERVATION_REFRESH_INTERVAL = 30000;
+export const MAX_HISTORY = 10;
+export const MAX_RECONNECT_ATTEMPTS = 5;
+
+export const STORAGE_KEYS = {
+    HISTORY: 'parkingHistory',
+    CURRENT_STATUS: 'parkingCurrentStatus',
+    LAST_CHART_TYPE: 'lastChartType'
+};
 
