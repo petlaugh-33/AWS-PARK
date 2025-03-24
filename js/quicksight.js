bb390c08baa0272780961d1401fb400405f36c67
slickediscool <edramirr@amazon.com>
2025-03-23 21:08:51 -0400

    new

--- js/quicksight.js
+++ js/quicksight.js
@@ -4,12 +4,11 @@ export function embedQuickSightDashboard(type = 'daily') {
             daily: "https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/160885289976/dashboards/f54f792c-9a8f-4d9e-b679-227e1c2257c3?directory_alias=AmazonPark-PeakTimes",
             weekly: "https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/160885289976/dashboards/953a2fba-f0fd-4d2f-bac5-218d43e6e866?directory_alias=AmazonPark-PeakTimes"
         };
-
         const container = document.getElementById('quicksight-dashboard');
         container.innerHTML = `
             <iframe
                 width="100%"
-                height="500px"
+                height="1000px"
                 src="${dashboardUrls[type]}"
                 frameborder="0"
                 allowfullscreen>
@@ -20,5 +19,4 @@ export function embedQuickSightDashboard(type = 'daily') {
     }
 }
 
-// Make available globally
 window.embedQuickSightDashboard = embedQuickSightDashboard;
