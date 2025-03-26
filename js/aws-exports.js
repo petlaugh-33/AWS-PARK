15992242b4b3002081c22e330069a2f4c35c2b9d
petlaugh-33 <petlaugh@amazon.com>
2025-03-26 10:54:10 -0400

    Create aws-exports.js

---
+++ js/aws-exports.js
@@ -0,0 +1,16 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_YK7Q60V8o",
+    "aws_user_pools_web_client_id": "1uh3r35aemfub1ee1lin6o8lb1",
+    "oauth": {},
+    "aws_cloud_logic_custom": [
+        {
+            "name": "api",
+            "endpoint": "https://wszwvwc915.execute-api.us-east-1.amazonaws.com/prod",
+            "region": "us-east-1"
+        }
+    ]
+};
+
+export default awsmobile;
